a7acff9213f1e33309247e7fa4ff10b88414b44a
jerry <17app001@gmail.com>
2022-05-12 15:18:54 +0800

    完成本息計算方式

---
+++ js/index.js
@@ -0,0 +1,74 @@
+const amountEl = document.querySelector("#amount");
+const priodEl = document.querySelector("#proid");
+const priodSelectEl = document.querySelector("#proid-select");
+const feeEl = document.querySelector("#fee");
+const rateEl = document.querySelector("#rate");
+const event1El = document.querySelector("#event-1");
+const event2El = document.querySelector("#event-2");
+
+priodSelectEl.addEventListener("change", () => {
+    priodEl.value = Number(priodSelectEl.options[priodSelectEl.selectedIndex].text);
+});
+
+// 本金平均攤還
+function event1(amount, priod, rate) {
+    let tempTotalAmount = amount;
+    let resultArray = new Array();
+
+    // 本金固定
+    let pay = Math.round(amount / priod);
+    for (let i = 0; i < priod; i++) {
+        // 每月利息
+        let interest = Math.round(tempTotalAmount * (rate / 12));
+        tempTotalAmount -= pay;
+
+        // resultArray.push([pay, interest, tempTotalAmount]);
+        resultArray[i] = new Array();
+        resultArray[i][0] = pay;
+        resultArray[i][1] = interest;
+        resultArray[i][2] = tempTotalAmount;
+    }
+
+    return resultArray;
+}
+
+
+function calc() {
+    try {
+        let amount = Number(amountEl.value) * 10000;
+        let priod = priodEl.value == ''
+            ? Number(priodSelectEl.options[priodSelectEl.selectedIndex].text)
+            : Number(priodEl.value);
+
+        let fee = feeEl.value == '' ? 0 : Number(feeEl.value);
+        let rate = rateEl.value == '' ? 0 : Number(rateEl.value);
+
+        if (amount == 0) {
+            alert("請輸入貸款金額");
+            return;
+        }
+
+        if (rate <= 0) {
+            alert("請輸入正確利率");
+            return;
+        }
+
+        let event = event1El.checked ? 0 : 1;
+
+        console.log(amount, priod, fee, rate, event);
+
+        let resultArray = event1(amount, priod * 12, rate / 100);
+
+        const resultEL = document.querySelector("#result");
+        resultEL.innerHTML = "";
+        let count = 0;
+        resultArray.forEach(result => {
+            resultEL.innerHTML += `<li>第${count + 1}個月\t ${result[0]} | ${result[1]} | ${result[2]}</li>`;
+            count++;
+
+        });
+
+    } catch (e) {
+        alert(e);
+    }
+}
